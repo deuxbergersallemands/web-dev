1568d666e779b2084f7af878001a37d10e0ff3a9
Saber <frejsaber@gmail.com>
2016-01-14 16:07:14 +0100

    modif app.js

--- app.js
+++ app.js
@@ -6,10 +6,53 @@ var Db = require('mongodb').Db,
 
 
 var db = new Db('deuxbergersallemands_web-dev', new Server('localhost', 27017));
-//var app=require("express")();
 var express=require("express");
-var app=express();
+/****************************************************/
+var bodyParser = require('body-parser')
+var md5 = require("md5")
 
+var url = "mongodb://localhost:27017/test"
+var app = express()
+
+app.use(bodyParser.json()) // Nécessaire pour parser le body des requêtes PUT (Q5)
+
+var getETag = function(body) {
+    return md5(body)
+}
+
+app.set("etag", getETag)
+//world_bank
+MongoClient.connect(url, function(err, db) {
+    db.collection("Utilisateur", function(err, Utilisateur) {
+            app.use(express.static(__dirname+'/client'));
+            app.use("/tableauDeBord",function(){
+                console.log("/tableauDeBord");
+            })
+
+        app.get("/projects", function (req, resp, next) { // Question 2
+           
+
+            var cursor = Utillisateur.find().limit(2)
+            cursor.toArray(function(err, data) {
+                if (err) return next(err)
+                resp.json(data)
+            })
+        })
+
+
+   
+
+        app.listen(3000, function() {
+            console.log("Server running...")
+        })
+    })
+})
+
+
+
+/******************************************************/
+
+/*
 
 
 // Establish connection to db
@@ -19,12 +62,12 @@ db.open(
             console.log("connexion reussite au serveur base de données réussite  localhost', 27017");
             console.log("demarrage de serveur http l'application localhost:3000 ");
             app.use(express.static(__dirname+'/client'));
+            app.use("/tableauDeBord",function(){
+                console.log("/tableauDeBord");
+//                var cursor =db.Utilisateur.find().
 
-            /*
-            app.use(function(req,res){
-                 res.sendFile(__dirname+'/client/index.html');
+            })
 
-            });*/
            
 
             app.listen(3000);
@@ -35,3 +78,4 @@ db.open(
         
 }
 );
+*/
