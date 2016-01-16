d9b487455f16b75b5645ed58af18e4b9a2cdd4a5
Netty Larisse <nlarisse@emi.u-bordeaux1.fr>
2016-01-16 15:53:47 +0100

    ajouter une transaction

--- client/app/composants/transaction/nouvelle/transactionNouvelleControleur.js
+++ client/app/composants/transaction/nouvelle/transactionNouvelleControleur.js
@@ -1,4 +1,18 @@
 myApp.controller('TransactionNouvelleControleur', ['$scope', '$route', '$routeParams', '$location', function($scope, $route, $routeParams, $location) {
 
   $scope.historique = [{texte:'Transaction1', date:'la date'}, {texte:'Transaction1', date:'la date'}];
+  
+  $scope.noms = [];
+
+  $scope.nomAjouter = "";
+
+  $scope.ajouterNom = function() {
+  	$scope.noms.push($scope.nomAjouter);
+  	$scope.nomAjouter = "";
+  }
+
+  $scope.sauvegarder =function() {
+  	console.log("sauvegarder");
+  }
+
 }]);
