9d7a7c6a944b6be80d479b1e1331cb17881965b4
Tristan Campbell <tcampbel@ucsd.edu>
2016-01-18 20:22:46 +0100

    Cookies

--- client/app/composants/amis/ajouterAmisControleur.js
+++ client/app/composants/amis/ajouterAmisControleur.js
@@ -1,12 +1,21 @@
 myApp.controller('AjouterAmisControleur', ['$scope', '$route', '$routeParams', '$location', '$cookies', 'Amis', function($scope, $route, $routeParams, $location, $cookies, Amis) {
 
 
-  $scope.noms = [{nom:'Nathy', prenom:'Moimeme'}, {nom:'Jolie', prenom:'Nature'}];
+  $scope.noms;
 
   $scope.recupererAmis = function() {
-    $scope.amis = new Amis();
-
+  	console.log('qpppel');
+    $scope.amis = [];
+    $scope.amis = new Amis().$query(function (transaction, headers) {
+                   console.log(transaction);
+                        // succès
+                }, function (error) {
+                	console.log("erreur");
+                    // échec
+                });
   }
+ 
+  $scope.recupererAmis();
 
   $scope.nomAmi = "";
   $scope.ajouterAmi = function() {
