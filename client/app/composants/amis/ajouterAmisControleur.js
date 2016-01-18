9e5a59dc77c871ba85d6997dd58dc43accf5035c
Netty Larisse <nlarisse@emi.u-bordeaux1.fr>
2016-01-18 20:07:49 +0100

    lsite amis

--- client/app/composants/amis/ajouterAmisControleur.js
+++ client/app/composants/amis/ajouterAmisControleur.js
@@ -1,16 +1,25 @@
 myApp.controller('AjouterAmisControleur', ['$scope', '$route', '$routeParams', '$location', '$cookies', 'Amis', function($scope, $route, $routeParams, $location, $cookies, Amis) {
 
 
-  $scope.noms = [{nom:'Nathy', prenom:'Moimeme'}, {nom:'Jolie', prenom:'Nature'}];
 
   $scope.recupererAmis = function() {
+  	console.log("apppelll");
     $scope.amis = new Amis();
-
+    $scope.amis.$query(function (transaction, headers) {
+                  	console.log("dddd");
+                        // succès
+                }, function (error) {
+                	console.log("erreur");
+                    // échec
+                });
   }
 
+    $scope.noms = $scope.recupererAmis();
+
+ 
   $scope.nomAmi = "";
   $scope.ajouterAmi = function() {
-  	$scope.noms.push($scope.nomAmi);
-  	$scope.nomAmi = "";
+  //	$scope.noms.push($scope.nomAmi);
+  //	$scope.nomAmi = "";
   }
 }]);
--- client/app/partages/navbarConnecte/NavbarConnecteControleur.js
+++
@@ -1,8 +0,0 @@
-myApp.controller('NavbarConnecteContoleur', ['$scope', '$route', '$routeParams', '$location', '$cookies', function($scope, $route, $routeParams, $location, $cookies) {
-  
-  $scope.deconnecter = function() {
-    $cookies.remove('utilisateur');
-    $location.path('/');
-  }
-
-}]);
