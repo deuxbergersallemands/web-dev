ef309f1a1970c1f890daacf8185a0cbdc8e009cb
Tristan Campbell <tcampbel@ucsd.edu>
2016-01-20 14:18:23 +0100

    Groupes récupérés

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ MongoClient.connect(url, function(err, db) {
 
             app.get('/tableauDeBord/Transactions/Participant', function(req, res) {
                 var users=Transaction.find({'participants.participant.mel' : req.cookies.utilisateur}).toArray( function(err, users) {
-                    if (err) return next(err)
+                    if (err) return res.send(err);
                     if (users == null)
                       res.status(404).end()
                     else{
@@ -215,23 +215,21 @@ MongoClient.connect(url, function(err, db) {
                     console.log("resultat introuvable : app.get /groupe/One ")
             }
         });
-
-        app.get('/groupe',function(req,res){
-            if(req!=null){
-                var result =Groupe.find("req")
-                if(result!=null){
-                    res.result=result;
-                    res.send();
+ 
+        // Récupérer tous les groups dont l'utilisateur fait parti.
+        app.get('/groupes',function(req,res){
+            Groupe.find({'membres.mel' : req.cookies.utilisateur}).toArray( function(err, groupes) {
+              if (err) res.send(err);
+              if (groupes == null) {res.send(err);}
+              else {
+                    res.send(groupes);
                 }
-                else
-                    console.log("reultat introuvable : app.get /groupe ")
-            }
         });
+    })
     
         app.post('/groupe/nouvelle', function(req, res) {             
             Groupe.insert(req.body);
             res.send();
-
         });
     })
 
